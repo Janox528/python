ed318add3a83f19ef79b4d6c722118a3eafb1857
Janox528 <61915272+Janox528@users.noreply.github.com>
2020-03-25 16:51:29 +0100

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -17,24 +17,25 @@ class Board:
                     row_temp += symbol + " "
             print(row_temp)
 
-def checkWinCondition(board,player):
-    # Check rows
-    # player = 'X' oder 'O'
+    def checkWinCondition(self,player):
+        # Check rows
+        # player = 'X' oder 'O'
 
-    if [player]*board.size in board.grid:
-        return True
-    
-
-    # Check columns
-    for j in range(board.size):
-        if [board.grid[i][j] for i in range(board.size)] == [player]*board.size:
+        if [player]*self.size in self.grid:
             return True
+        
 
-    # Check diagonal \
-    if [board.grid[i][i] for i in range(board.size)] == [[player]*board.size] or \
-        [board.grid[board.size-1-i][i] for i in range(board.size)] == [[player]*board.size]:
-        return True
-    return False
+        # Check columns
+        for j in range(self.size):
+            if [self.grid[i][j] for i in range(self.size)] == [player]*self.size:
+                return True
+
+        # Check diagonal \
+        if [self.grid[i][i] for i in range(self.size)] == [[player]*self.size] or \
+            [self.grid[self.size-1-i][i] for i in range(self.size)] == [[player]*self.size]:
+            return True
+        
+        return False
 
 
 
@@ -51,7 +52,7 @@ def main():
     board.grid[2][2] = 'X'
     board.grid[2][0] = 'O'
     board.displayBoard()
-    checkWinCondition(board,'X')
+    print(board.checkWinCondition('O'))
 
 if __name__ == '__main__':
     main()
